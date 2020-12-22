45c4bdde47966244fa5c76984173a8c0662f44fb
Rob Bultman <rob.bultman@gmail.com>
2020-12-22 12:46:42 -0500

    Added LCD pins, datasheet

--- source/main.cpp
+++ source/main.cpp
@@ -13,15 +13,25 @@ DigitalOut redLed(LED1);
 DigitalOut greenLed(LED2);
 DigitalOut blueLed(LED3);
 DigitalOut redLed2(LED4);
+DigitalOut lcdCS(D10);
+DigitalOut lcdDC(D9);
+DigitalOut lcdReset(D8);
+DigitalOut lcdBacklight(D5);
+DigitalOut touchCS(D7);
+DigitalIn  touchInt(D6);
+
+SPI spi(D11, D12, D13);
 
 int main()
 {
    while (true) 
    {
       greenLed = 1;
+      lcdBacklight = 1;
       ThisThread::sleep_for(BLINKING_RATE);
       
       greenLed = 0;
+      lcdBacklight = 0;
       ThisThread::sleep_for(BLINKING_RATE);
    }
 }
